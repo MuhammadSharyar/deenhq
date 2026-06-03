efdd51264aa4132f5fa9c7ac1e578d1329b5c676
Muhammad Sharyar <muhammad.sharyar@tycheros.net>
2026-06-03 14:56:45 +0400

    fix: generated new PWA icons based on new UI theme

--- vite.config.ts
+++ vite.config.ts
@@ -13,16 +13,21 @@ export default defineConfig({
     VitePWA({
 
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'favicon.svg'],
       manifest: {
         name: 'DeenHQ',
         short_name: 'DeenHQ',
         description: 'Privacy-first Islamic Utility PWA',
-        theme_color: '#0070F2',
+        theme_color: '#0d9488',
         background_color: '#f8fafc',
         display: 'standalone',
         start_url: '/',
         icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
@@ -32,6 +37,12 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       }
